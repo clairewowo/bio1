feaed98202c6360ecdcb8bc4af266198629576e1
clairewowo <claire.ze.wang@gmail.com>
2024-07-07 17:31:03 -0700

    first

---
+++ bio1-2.py
@@ -0,0 +1,8 @@
+def PatternCount(text, pattern):
+	count = 0
+	for i in range(0, len(text) - len(pattern)):
+		if text[i : i + len(pattern)] == pattern:
+			count += 1
+	return count
+
+PatternCount("aaa", "a")
